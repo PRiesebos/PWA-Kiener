ac22fcaf7b58d48baaecb27628910f2da63941b5
PRiesebos <priesebos@live.com>
2019-03-25 14:01:09 +0100

    Removed firebase dev warning

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import firebase from "firebase";
+import firebase from "firebase/app";
 import "./registerServiceWorker";
 import "./scss/custom.scss";
 import "./scss/style.scss";
