7ea49df5a735aa661736cfde9343824779875df2
PRiesebos <priesebos@live.com>
2019-04-02 13:59:49 +0200

    Added search icon

--- src/main.js
+++ src/main.js
@@ -10,11 +10,11 @@ import "jquery";
 import "bootstrap";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faAlignJustify } from "@fortawesome/free-solid-svg-icons";
+import { faAlignJustify, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(faAlignJustify, faUser);
+library.add(faAlignJustify, faSearch, faUser);
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 
